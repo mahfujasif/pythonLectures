1ceb002305a497e83ba581bfd6a00ac64c3d6562
asif <mahfujasif@gmail.com>
2023-11-28 21:28:06 +0000

    Season 2 added

--- fund2/1_lecture.py
+++ fund2/1_lecture.py
@@ -34,11 +34,62 @@
 # a = b = 5
 # print(a,b)
 
-#equality
-a_f = 5.5
-b_f = 5.5
-c_f = b_f
-print(a_f == b_f)
-print(a_f is b_f)
-print(b_f == c_f)
-print(b_f is c_f)
+# #equality
+# a_f = 5.5
+# b_f = 5.5
+# c_f = b_f
+# print(a_f == b_f)
+# print(a_f is b_f)
+# print(b_f is a_f)
+# print(b_f == c_f)
+# print(b_f is c_f)
+
+# # type conversion
+# for i in range(5):
+#     f = i+2.4
+#     v = "this is "+ str(i)+ "th el with val " + str(f)
+#     print(v)
+
+# p = int(float('2.1'))
+# print(p)
+# f = float('2')
+# print(f)
+# s = str("2.1")
+# print(s)
+
+# #chain statement
+# myInt = 4
+# print(0 < myInt <= 5 > 1)
+
+# p = 1.1+2.2
+# print(p)
+
+# u =  11111113
+# v = -11111111
+# w = 7.511111
+# x = (u + v) + w
+# y = u + (v + w)
+# print(x)
+# print(y)
+# print(x == y)
+# close_enough_level = 0.0000001
+# print((x-y) < close_enough_level)
+
+# x, y = 5, 5
+# if x-y:
+#     print("Non zero")
+# else:
+#     print("Zero")
+#
+# arr = []
+# if(arr):
+#     print("Non Empty")
+# else:
+#     print("Empty")
+#
+# s = ""
+# if s:
+#     print("Non Empty String")
+# else:
+#     print("Empty String")
+
